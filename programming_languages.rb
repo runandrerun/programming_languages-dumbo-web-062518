dda9272fa1d8e3175b5edb1f5544cbe281959d3c
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-21 00:53:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
-def reformat_languages(languages)
-  # your code here
-end
+def reformat_languages(language_hash)
+  new_hash = {}
+  language_hash.each do |language_cat, languages|
+    languages.each do |language, attributes|
+      attributes.each do |attribute, att_value|
+        if language == :javascript
+          new_hash[language] = {
+            :type => att_value,
+            :style => [:oo, :functional]
+          }
+        else
+          new_hash[language] = {
+             :type => att_value,
+             :style => [language_cat]
+          }
+        end
+      end
+    end
+  end
+  return new_hash
+ end	 
