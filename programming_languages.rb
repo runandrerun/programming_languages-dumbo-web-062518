3688a93510e96c537d2b35f2b384364ab8c3a61d
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-21 00:56:17 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,15 @@
 def reformat_languages(language_hash)
-  new_hash = {}
-  language_hash.each do |language_cat, languages|
-    languages.each do |language, attributes|
-      attributes.each do |attribute, att_value|
-        if language == :javascript
+  new_hash = {}  # Create a new hash to store data
+  language_hash.each do |language_cat, languages| # Start iteration process over level 1
+    languages.each do |language, attributes| # continue iteration to level 2
+      attributes.each do |attribute, att_value| # iterate on level 3 to attributes
+        # if the language (key of level 2) has a key of :javascript create new hash
+        if language == :javascript 
           new_hash[language] = {
             :type => att_value,
             :style => [:oo, :functional]
           }
+        # else create a new hash for any language 
         else
           new_hash[language] = {
              :type => att_value,
@@ -17,5 +19,6 @@ def reformat_languages(language_hash)
       end
     end
   end
+  # return the new_hash
   return new_hash
  end	 
